38dd5788e9ec681c524d311c454456e596e87c3e
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2018-04-03 02:55:09 -0700

    command line option to retrieve user address and encode as a url with http request to googlegeocode api successful

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const request = require('request')
+const yargs = require('yargs')
 
+//yargs to retrieve address from user
+const argv = yargs.command('locate', 'Find your geocode', {
+    addr: {
+        alias: 'a',
+        demand: true,
+        describe: '<address>'
+    }
+}).help().argv
+
+//urlencode the address
+urlAddress = encodeURI(argv.addr)
+
+//make a request for the geocode of that address to google geocode api
+//note that adding json:true property parses the JSON to an object for me and ensures that the body is defined
 request({
-    url: 'http://maps.googleapis.com/maps/api/geocode/json?address=10673%20blue%20nile%20ct',
+    url: 'http://maps.googleapis.com/maps/api/geocode/json?address='+urlAddress,
+    json: true
 }, (error, response, body) => {
-    let coordinates = JSON.parse(body).results[0].geometry.location
-    console.log(coordinates)
+    let geocode = body.results[0]
+    let coordinates = geocode.geometry.location
+
+    console.log(`Address: ${geocode.formatted_address}`)
+    console.log(`Latitude: ${coordinates.lat}`)
+    console.log(`Longitude: ${coordinates.lng}`)
 })
