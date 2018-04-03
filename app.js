01d22ff1f7110576bfaef1b128660cee79948bc7
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2018-04-03 03:09:57 -0700

    restructued yargs with .option since app only takes one command

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const request = require('request')
 const yargs = require('yargs')
 
-//yargs to retrieve address from user
-const argv = yargs.command('locate', 'Find your geocode', {
-    addr: {
-        alias: 'a',
-        demand: true,
-        describe: '<address>'
-    }
-}).help().argv
-
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    }).help()
+    .alias('help', 'h')
+    .argv
+    
 //urlencode the address
-urlAddress = encodeURI(argv.addr)
+urlAddress = encodeURI(argv.a)
 
 //make a request for the geocode of that address to google geocode api
 //note that adding json:true property parses the JSON to an object for me and ensures that the body is defined
@@ -19,6 +22,7 @@ request({
     url: 'http://maps.googleapis.com/maps/api/geocode/json?address='+urlAddress,
     json: true
 }, (error, response, body) => {
+
     let geocode = body.results[0]
     let coordinates = geocode.geometry.location
 
